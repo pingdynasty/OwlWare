6b57a706890f406ff825343361f5f3c58115b6b5
mars <martin@rebel-it.com>
2014-03-12 13:36:06 +0000

    added new and delete array operators

--- Source/operators.cpp
+++ Source/operators.cpp
@@ -4,6 +4,8 @@ extern "C" void *__gxx_personality_v0;
 extern "C" void __cxa_end_cleanup (void);
 extern "C" void __cxa_pure_virtual(){}
 
-void* operator new(size_t size) { return malloc(size); }
-void operator delete(void* ptr) { free(ptr); }
+void * operator new(size_t size) { return malloc(size); }
 void * operator new (size_t, void * p) { return p ; }
+void * operator new[](size_t size) { return malloc(size); }
+void operator delete(void* ptr) { free(ptr); }
+void operator delete[](void * ptr) { free(ptr); }
