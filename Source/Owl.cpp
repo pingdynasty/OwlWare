d74f93f204f6e94a22fdbe9f60f1fba6a3f23163
mars <martin@rebel-it.com>
2014-04-25 17:48:20 +0100

    added cycle count to debug_audio

--- Source/Owl.cpp
+++ Source/Owl.cpp
@@ -81,12 +81,19 @@ volatile bool collision = false;
 uint16_t* source;
 uint16_t* dest;
 
+static int count = 0;
 __attribute__ ((section (".coderam")))
 void run(){
   for(;;){
     if(doProcessAudio){
 #ifdef DEBUG_AUDIO
       setPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
+      volatile unsigned int *DWT_CYCCNT = (volatile unsigned int *)0xE0001004; //address of the register
+      volatile unsigned int *DWT_CONTROL = (volatile unsigned int *)0xE0001000; //address of the register
+      volatile unsigned int *SCB_DEMCR = (volatile unsigned int *)0xE000EDFC; //address of the register
+      *SCB_DEMCR = *SCB_DEMCR | 0x01000000;
+      *DWT_CYCCNT = 0; // reset the counter
+      *DWT_CONTROL = *DWT_CONTROL | 1 ; // enable the counter
 #endif
       buffer.split(source);
       patches.process(buffer);
@@ -101,6 +108,7 @@ void run(){
       }
 #ifdef DEBUG_AUDIO
       clearPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
+      count = *DWT_CYCCNT;
 #endif
     }
   }
