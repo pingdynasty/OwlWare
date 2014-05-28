4d8bca22b12a3e54f3f283c6423169c71dd6cb3c
mars <martin@rebel-it.com>
2014-05-28 13:18:59 +0100

    added dwt cycle counter

--- Source/Owl.cpp
+++ Source/Owl.cpp
@@ -76,12 +76,19 @@ volatile bool collision = false;
 uint16_t* source;
 uint16_t* dest;
 
+extern uint32_t dwt_count = 0;
 __attribute__ ((section (".coderam")))
 void run(){
   for(;;){
     if(doProcessAudio){
 #ifdef DEBUG_AUDIO
       setPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
+      volatile unsigned int *DWT_CYCCNT = (volatile unsigned int *)0xE0001004; //address of the register
+      volatile unsigned int *DWT_CONTROL = (volatile unsigned int *)0xE0001000; //address of the register
+      volatile unsigned int *SCB_DEMCR = (volatile unsigned int *)0xE000EDFC; //address of the register
+      *SCB_DEMCR = *SCB_DEMCR | 0x01000000;
+      *DWT_CYCCNT = 0; // reset the counter
+      *DWT_CONTROL = *DWT_CONTROL | 1 ; // enable the counter
 #endif
       buffer.split(source);
       patches.process(buffer);
@@ -96,6 +103,7 @@ void run(){
       }
 #ifdef DEBUG_AUDIO
       clearPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
+      dwt_count = *DWT_CYCCNT;
 #endif
     }
   }
