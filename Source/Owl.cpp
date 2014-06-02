19e78aba6a05bc76fee6a1b7ce5e42c982829f6f
mars <martin@rebel-it.com>
2014-06-02 12:29:49 +0100

    integrated analytics

--- Source/Owl.cpp
+++ Source/Owl.cpp
@@ -93,12 +93,6 @@ void run(){
     if(doProcessAudio){
 #ifdef DEBUG_AUDIO
       setPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
-      volatile unsigned int *DWT_CYCCNT = (volatile unsigned int *)0xE0001004; //address of the register
-      volatile unsigned int *DWT_CONTROL = (volatile unsigned int *)0xE0001000; //address of the register
-      volatile unsigned int *SCB_DEMCR = (volatile unsigned int *)0xE000EDFC; //address of the register
-      *SCB_DEMCR = *SCB_DEMCR | 0x01000000;
-      *DWT_CYCCNT = 0; // reset the counter
-      *DWT_CONTROL = *DWT_CONTROL | 1 ; // enable the counter
 #endif
 #ifdef DEBUG_DWT
       *DWT_CYCCNT = 0; // reset the counter
@@ -116,7 +110,6 @@ void run(){
       }
 #ifdef DEBUG_AUDIO
       clearPin(GPIOC, GPIO_Pin_5); // PC5 DEBUG
-      count = *DWT_CYCCNT;
 #endif
 #ifdef DEBUG_DWT
       dwt_count = *DWT_CYCCNT;
