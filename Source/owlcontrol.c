83409347aec22b49a5878cdffefcb48791b18089
mars <martin@rebel-it.com>
2014-06-14 21:01:56 +0100

    changed magick

--- Source/owlcontrol.c
+++ Source/owlcontrol.c
@@ -44,7 +44,7 @@ uint8_t* getDeviceId(){
 /* Magic number to store in memory before reset to tell the device to
  * enter system bootloader.
  */
-const uint32_t kBootloaderMagicNumber = 0x5EA1AB50;
+const uint32_t kBootloaderMagicNumber = 0xF00B4400;
 
 /* Jump to the internal STM32 bootloader. The way this works is that we
  * set a magic number in memory that our startup code looks for (see startup_stm32f4xx.s).
