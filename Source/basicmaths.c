fc864700e1b471d58e0c53658439870fda3c4ed6
mars <martin@rebel-it.com>
2014-09-28 02:39:15 +0100

    added some fastsqrt functions and M_SQRT2 constant

--- Source/basicmaths.c
+++ Source/basicmaths.c
@@ -1,4 +1,5 @@
 #include "basicmaths.h"
+#include <stdint.h>
 
 float fastPow(float a, float b){
   /* Taken from
@@ -11,4 +12,37 @@ float fastPow(float a, float b){
   u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
   u.x[0] = 0;
   return u.d;
+}
+
+// Faster approximations to sqrt()
+//From http://ilab.usc.edu/wiki/index.php/Fast_Square_Root
+//The faster the routine, the more error in the approximation.
+
+// Log Base 2 Approximation
+// 5 times faster than sqrt()
+
+float fastsqrt1( float x ) {
+  union { int32_t i; float x; } u;
+  u.x = x;
+  u.i = ((int32_t)1<<29) + (u.i >> 1) - ((int32_t)1<<22); 
+  return u.x;
+}
+
+// Log Base 2 Approximation with one extra Babylonian Step
+// 2 times faster than sqrt()
+
+float fastsqrt2( float x ) {
+  float v=fastsqrt1( x );
+  v = 0.5f * (v + x/v); // One Babylonian step
+  return v;
+}
+
+// Log Base 2 Approximation with two extra Babylonian Steps
+// 50% faster than sqrt()
+
+float fastsqrt3( float x ) {
+  float v=fastsqrt1( x );
+  v =v + x/v;
+  v = 0.25f* v + x/v; // Two Babylonian steps
+  return v;
 }
