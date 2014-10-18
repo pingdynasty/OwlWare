3c9c7eaa49a4ff4fd26c8c431ee87768bd8353ef
mars <martin@rebel-it.com>
2014-10-18 23:25:41 +0100

    draft firmware sender code

---
+++ sender/FirmwareSender.cpp
@@ -0,0 +1,225 @@
+/*
+  g++ -std=c++11 -Isender sender/FirmwareSender.cpp Source/sysex.c ../OwlNest/JuceLibraryCode/modules/juce_core/juce_core.cpp ../OwlNest/JuceLibraryCode/modules/juce_audio_basics/juce_audio_basics.cpp ../OwlNest/JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.cpp ../OwlNest/JuceLibraryCode/modules/juce_events/juce_events.cpp -lpthread -ldl -lX11 -lasound
+*/
+#include <unistd.h>
+#include <stdint.h>
+#include <math.h>
+#include "../../OwlNest/JuceLibraryCode/JuceHeader.h"
+#include "../Source/OpenWareMidiControl.h"
+#include "../Source/CRCC.hpp"
+#include "../Source/sysex.h"
+#include "../Source/MidiStatus.h"
+
+class CommandLineException : public std::exception {
+private:
+  juce::String cause;
+public:
+  CommandLineException(juce::String c) : cause(c) {}
+  juce::String getCause() const {
+    return cause;
+  }
+  const char* what() const noexcept {
+    return getCause().toUTF8();
+  }
+};
+
+class FirmwareUploadApplication {
+private:
+  bool running = false;
+  bool verbose = true;
+  juce::ScopedPointer<MidiOutput> midiout;
+  juce::ScopedPointer<File> fileout;
+  juce::ScopedPointer<File> input;
+  juce::ScopedPointer<OutputStream> out;
+public:
+  void listDevices(const StringArray& names){
+    for(int i=0; i<names.size(); ++i)
+      std::cout << i << ": " << names[i] << std::endl;
+  }
+
+  MidiOutput* openMidiOutput(const String& name){
+    MidiOutput* output = NULL;    
+    StringArray outputs = MidiOutput::getDevices();
+    for(int i=0; i<outputs.size(); ++i){
+      if(outputs[i].trim().matchesWildcard(name, true)){
+	if(verbose)
+	  std::cout << "opening MIDI output " << outputs[i] << std::endl;
+	output = MidiOutput::openDevice(i);
+	break;
+      }
+    }
+    if(output != NULL)
+      output->startBackgroundThread();
+    return output;
+  }
+
+  void send(MemoryBlock& block){
+    send((unsigned char*)block.getData(), block.getSize());
+  }
+
+  void send(unsigned char* data, int size){
+    if(verbose)
+      std::cout << "sending " << size << " bytes" << std::endl;
+    if(out != NULL){
+      out->writeByte(SYSEX);
+      out->write(data, size);
+      out->writeByte(SYSEX_EOX);
+      out->flush();
+    }
+    if(midiout != NULL)
+      midiout->sendMessageNow(juce::MidiMessage::createSysExMessage(data, size));
+  }
+
+  void usage(){
+    std::cerr << getApplicationName() << std::endl 
+	      << "usage:" << std::endl
+	      << "-h or --help\tprint this usage information and exit" << std::endl
+	      << "-l or --list\tlist available MIDI ports and exit" << std::endl
+	      << "-in FILE\tinput FILE" << std::endl
+	      << "-out DEVICE\tsend output to MIDI interface DEVICE" << std::endl
+	      << "-save FILE\twrite output to FILE" << std::endl
+      ;
+  }
+
+  void configure(int argc, char* argv[]) {
+    for(int i=1; i<argc; ++i){
+      juce::String arg = juce::String(argv[i]);
+      if(arg.compare("-h") == 0 || arg.compare("--help") == 0 ){
+	usage();
+	throw CommandLineException(juce::String::empty);
+      }else if(arg.compare("-q") == 0){
+	verbose = false;
+      }else if(arg.compare("-l") == 0 || arg.compare("--list") == 0){
+	std::cout << "MIDI input devices:" << std::endl;
+	listDevices(MidiInput::getDevices());
+	std::cout << "MIDI output devices:" << std::endl;
+	listDevices(MidiOutput::getDevices());
+	throw CommandLineException(juce::String::empty);
+      }else if(arg.compare("-in") == 0 && ++i < argc){
+	juce::String name = juce::String(argv[i]);
+	input = new juce::File(name);
+      }else if(arg.compare("-out") == 0 && ++i < argc){
+	juce::String name = juce::String(argv[i]);
+	midiout = openMidiOutput(name);
+      }else if(arg.compare("-save") == 0 && ++i < argc){
+	juce::String name = juce::String(argv[i]);
+	fileout = new juce::File(name);
+	fileout->deleteFile();
+	fileout->create();
+      }else{
+	usage();
+	throw CommandLineException(juce::String::empty);
+      }
+    }
+    if(input == NULL || (midiout == NULL && fileout == NULL)){
+      usage();
+      throw CommandLineException(juce::String::empty);
+    }
+  }
+
+  void run(){
+    running = true;
+    if(verbose){
+      std::cout << "Sending file " << input->getFileName() << std::endl; 
+      if(midiout != NULL)
+	std::cout << "\tto MIDI output" << std::endl; 
+      if(fileout != NULL)
+	std::cout << "\tto SysEx file " << fileout->getFullPathName() << std::endl;       
+    }
+    char header[] =  { 0x00, MIDI_SYSEX_MANUFACTURER, MIDI_SYSEX_DEVICE, SYSEX_FIRMWARE_UPLOAD };
+    int blocksize = 64;
+
+    InputStream* in = input->createInputStream();
+    if(fileout != NULL)
+      out = fileout->createOutputStream();
+      
+    // MemoryBlock block;
+    // block.append(header, sizeof(header));
+    MemoryOutputStream stream;
+    stream.write(header, sizeof(header));
+
+    unsigned char buffer[blocksize];
+    unsigned char sysex[(int)ceil(blocksize*8/7)];
+    int size = input->getSize() + 4; // include space for checksum
+    buffer[3] = (uint8_t)size & 0xff;
+    buffer[2] = (uint8_t)(size >> 8) & 0xff;
+    buffer[1] = (uint8_t)(size >> 16) & 0xff;
+    buffer[0] = (uint8_t)(size >> 24) & 0xff;
+    int len = data_to_sysex(buffer, sysex, 4);
+    if(len != 5)
+      throw CommandLineException("Error in sysex conversion"); 
+    stream.write(sysex, len);
+    // block.append(sysex, len);
+
+    CRCC crc;
+    uint32_t checksum = 0;
+    size = input->getSize(); // amount of data, excluding checksum
+    for(int i=0; i < size && running;){
+      len = in->read(buffer, blocksize);
+      checksum = crc.calc(len, buffer, checksum);
+      i += len;
+      if(verbose)
+	std::cout << "preparing " << len;
+      len = data_to_sysex(buffer, sysex, len);
+      if(verbose)
+	std::cout << "/" << len << " bytes binary/sysex" << std::endl;
+      stream.write(sysex, len);
+      // block.append(sysex, len);
+      if(i == size){
+	// last block
+	if(verbose)
+	  std::cout << "checksum 0x" << std::hex << checksum << std::endl;
+	buffer[3] = (uint8_t)checksum & 0xff;
+	buffer[2] = (uint8_t)(checksum >> 8) & 0xff;
+	buffer[1] = (uint8_t)(checksum >> 16) & 0xff;
+	buffer[0] = (uint8_t)(checksum >> 24) & 0xff;
+	len = data_to_sysex(buffer, sysex, 4);
+	if(len != 5)
+	  throw CommandLineException("Error in sysex conversion"); 
+	// block.append(sysex, len);
+	stream.write(sysex, len);
+	send((unsigned char*)stream.getData(), stream.getDataSize());
+      }else{
+	send((unsigned char*)stream.getData(), stream.getDataSize());
+	stream.reset();
+	stream.write(header, sizeof(header));
+      }
+      // send(block);
+      // block = MemoryBlock();
+      // block.append(header, sizeof(header));
+    }
+    stop();
+  }
+
+  void stop(){
+    if(midiout != NULL)
+      midiout->stopBackgroundThread();
+    if(out != NULL)
+      out->flush();
+  }
+
+  void shutdown(){
+    running = false;
+  }
+};
+
+FirmwareUploadApplication app;
+
+void sigfun(int sig){
+  std::cout << "shutting down" << std::endl;
+  app.shutdown();
+  (void)signal(SIGINT, SIG_DFL);
+}
+
+int main(int argc, char* argv[]) {
+  (void)signal(SIGINT, sigfun);
+  int status = 0;
+  try{
+    app.configure(argc, argv);
+    app.run();
+  }catch(const std::exception& exc){
+    std::cerr << exc.what() << std::endl;
+    status = -1;
+  }
+  return status;
+}
