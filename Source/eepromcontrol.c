9525d595d8d30b1228a02abbf84dc3a172a431f7
mars <martin@rebel-it.com>
2015-03-13 18:20:56 +0000

    fixed return value of erase

--- Source/eepromcontrol.c
+++ Source/eepromcontrol.c
@@ -67,33 +67,34 @@ int eeprom_erase(uint32_t address){
   if(address < 0x08004000)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_0, VoltageRange_3); */
-  else if(address < 0x08008000)
+  else if(address < ADDR_FLASH_SECTOR_2)
     FLASH_EraseSector(FLASH_Sector_1, VoltageRange_3);
-  else if(address < 0x0800C000)
+  else if(address < ADDR_FLASH_SECTOR_3)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_2, VoltageRange_3); */
-  else if(address < 0x08010000)
+  else if(address < ADDR_FLASH_SECTOR_4)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_3, VoltageRange_3); */
-  else if(address < 0x08020000)
+  else if(address < ADDR_FLASH_SECTOR_5)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_4, VoltageRange_3); */
-  else if(address < 0x08040000)
+  else if(address < ADDR_FLASH_SECTOR_6)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_5, VoltageRange_3); */
-  else if(address < 0x08060000)
+  else if(address < ADDR_FLASH_SECTOR_7)
     return -1;
     /* FLASH_EraseSector(FLASH_Sector_6, VoltageRange_3); */
-  else if(address < 0x08080000)
+  else if(address < ADDR_FLASH_SECTOR_8)
     FLASH_EraseSector(FLASH_Sector_7, VoltageRange_3);
-  else if(address < 0x080A0000)
+  else if(address < ADDR_FLASH_SECTOR_9)
     FLASH_EraseSector(FLASH_Sector_8, VoltageRange_3);
-  else if(address < 0x080C0000)
+  else if(address < ADDR_FLASH_SECTOR_10)
     FLASH_EraseSector(FLASH_Sector_9, VoltageRange_3);
-  else if(address < 0x080E0000)
+  else if(address < ADDR_FLASH_SECTOR_11)
     FLASH_EraseSector(FLASH_Sector_10, VoltageRange_3);
   else if(address < 0x08100000)
     FLASH_EraseSector(FLASH_Sector_11, VoltageRange_3);
   else
     return -1;
+  return 0;
 }
