700a497b5afbd0c326655109496f8b9bcfb44cfa
mars <martin@rebel-it.com>
2015-06-08 20:23:54 +0100

    added missing file

---
+++ Source/ProgramHeader.h
@@ -0,0 +1,24 @@
+#ifndef __PROGRAM_HEADER_H
+#define __PROGRAM_HEADER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+   struct ProgramHeader {
+     uint32_t magic;
+     uint32_t* linkAddress;
+     uint32_t* jumpAddress;
+     uint32_t* stackBegin;
+     uint32_t* stackEnd;
+     ProgramVector* programVector;
+     char programName[16];
+   };
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PROGRAM_HEADER_H */
