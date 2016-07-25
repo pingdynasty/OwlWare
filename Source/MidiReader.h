7d83c1a860b5b6a3cbf13176b30b8f96f1ed71f0
mars <martin@rebel-it.com>
2016-07-25 13:03:01 +0100

    unsigned pos and size

--- Source/MidiReader.h
+++ Source/MidiReader.h
@@ -13,8 +13,8 @@
 class MidiReader : public MidiHandler {
 protected:
   uint8_t buffer[MIDI_MAX_MESSAGE_SIZE];
-  static const int size = MIDI_MAX_MESSAGE_SIZE;
-  int pos;
+  static const unsigned int size = MIDI_MAX_MESSAGE_SIZE;
+  unsigned int pos;
 public:
   MidiReader() : pos(0) {}  
   void readMidiFrame(uint8_t* frame);
